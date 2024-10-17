167f62bd3401b43828e16e006471683146d2a8f6
marleneanton <the.marlene.anton@gmail.com>
2024-10-17 12:31:50 +0200

    creates method shells

--- backend/src/main/java/org/example/backend/PuzzleController.java
+++ backend/src/main/java/org/example/backend/PuzzleController.java
@@ -1,9 +1,20 @@
 package org.example.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/puzzles")
 public class PuzzleController {
+
+    @GetMapping
+    List<Puzzle> getRandomPuzzles(){
+        // todo: generate random three digit number
+        // todo: check that user hasn't completed either of the three yet
+        // return list of puzzles
+        return List.of(new Puzzle("01", "test puzz", "this is a test puzzle."));
+    }
 }
--- backend/src/main/java/org/example/backend/UserController.java
+++ backend/src/main/java/org/example/backend/UserController.java
@@ -1,9 +1,27 @@
 package org.example.backend;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @GetMapping("/{id}")
+    User getUserById(@PathVariable String id){
+        // TODO: get actual user
+        return new User("01", "test_user", "pw", Long.parseLong("1234"));
+    }
+
+    @GetMapping("/{id}/score")
+    Long getScoreById(@PathVariable String id){
+        // TODO: get actual score
+        return Long.parseLong("1234");
+    }
+
+    @PutMapping("/{id}/score")
+    User updateScoreById(@PathVariable String id){
+        // TODO: update actual score
+        return new User("01", "test_user", "pw", Long.parseLong("1234"));
+    }
+
+
 }
