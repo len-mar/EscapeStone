5af98d77ffac168d7a8baa21cf1d6ac7313ceb6a
marleneanton <the.marlene.anton@gmail.com>
2024-10-21 13:17:59 +0200

    adds completed puzzles to player

--- backend/src/main/java/org/example/backend/PlayerService.java
+++ backend/src/main/java/org/example/backend/PlayerService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,7 +18,7 @@ public class PlayerService {
     }
 
     Player createPlayer(PlayerDTO newPlayerDTO){
-    return playerRepo.save(new Player(UUID.randomUUID().toString(), newPlayerDTO.username(), newPlayerDTO.password(), Long.parseLong("0")));
+    return playerRepo.save(new Player(UUID.randomUUID().toString(), newPlayerDTO.username(), newPlayerDTO.password(), Long.parseLong("0"), List.of()));
     }
 
     Long getScoreById(String id){
