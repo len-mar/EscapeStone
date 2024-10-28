ce777ea940080ddf0db697d3ed1557f5be4800ab
marleneanton <the.marlene.anton@gmail.com>
2024-10-28 10:21:44 +0100

    adds primary colour change and mui theme setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,26 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#004e3a',  // Customize primary color
+        },
+        secondary: {
+            main: '#dc004e',  // Customize secondary color
+        },
+        // You can also customize other colors like `background`, `error`, etc.
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
+          <ThemeProvider theme={theme}>
           <App />
+          </ThemeProvider>
       </BrowserRouter>
   </StrictMode>,
 )
